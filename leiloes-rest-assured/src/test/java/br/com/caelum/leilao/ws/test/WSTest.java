d0352b2646b6762496fad245773cc693ddb0bff9
llcfromhell <llc.from.hell@gmail.com>
2016-09-30 07:31:56 -0300

    aula 04

---
+++ leiloes-rest-assured/src/test/java/br/com/caelum/leilao/ws/test/WSTest.java
@@ -0,0 +1,27 @@
+package br.com.caelum.leilao.ws.test;
+
+import static com.jayway.restassured.RestAssured.expect;
+
+import org.junit.Test;
+
+public class WSTest {
+
+	@Test
+	public void deveGerarUmCookie() {
+		
+		expect()
+			.cookie("rest-assured", "funciona")
+		.when()
+			.get("cookie/teste");
+		
+	}
+	
+	@Test
+	public void deveGerarUmHeader() {
+		expect()
+			.header("novo-header", "abc")
+		.when()
+			.get("/cookie/teste");
+	}
+	
+}
